ddf60e7219f8ecf6f29af9cba4558ae7b72955fd
romaric.guth <romaric.guth@epita.fr>
2021-11-04 21:47:19 +0100

    parse header and get needed fields indexes in csv

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 import pm2 from 'pm2';
 
+import {open, mkdir} from 'fs/promises';
+import {createReadStream, createWriteStream} from 'fs';
+import {splitCsvLine, splitCsvLineWithIndexes} from './csv_utils.js';
+
+const filehandle = await open('./csv/header.csv');
+const buf = await filehandle.readFile();
+filehandle.close();
+
+const fields =  splitCsvLine(buf);
+const needed = [
+  'siren',
+  'nic',
+  'siret',
+  'dateCreationEtablissement',
+  'dateDernierTraitementEtablissement',
+  'typeVoieEtablissement',
+  'libelleVoieEtablissement',
+  'codePostalEtablissement',
+  'libelleCommuneEtablissement',
+  'codeCommuneEtablissement',
+  'dateDebut',
+  'etatAdministratifEtablissement'
+];
+
+const indexes = needed.map((key) => fields.indexOf(key));
+console.log(splitCsvLineWithIndexes(buf, indexes));
+
+/*
 pm2.connect((err) => {
   if (err) {
     console.error(err)
     process.exit(2)
   }
 
-  const arr = ['csv-1'];
+  const nbfiles = 84;
 
-  for (let i = 0; i < arr.length; i += 1) {
+  for (let i = 0; i < nbfiles; i += 1) {
     pm2.start({
       script: './worker.js',
       name: 'demo',
-      args: [arr[i]]
+      args: i
     }, (err, apps) => {
         console.log(apps);
     });
   }
 });
+*/
